dfd2d29bfc779b6b83b4934b998c502466405ce1
jacekk <jacek.kapias@amsterdamstandard.com>
2019-09-29 00:12:28 +0200

    moved asssinging roles to users after creation;

--- rest-api/common/models/account.js
+++ rest-api/common/models/account.js
@@ -1,5 +1,38 @@
-'use strict';
+'use strict'
+
+const app = require('../../server/server')
+
+const ADMIN_EMAILS = ['admin@local.com']
+
+const ROLE_ADMIN = 'admin'
+const ROLE_CLIENT = 'client'
 
 module.exports = function(Account) {
+	Account.afterRemote('create', (ctx, entity, next) => {
+		const { Role, RoleMapping } = app.models
+		const roleName = ADMIN_EMAILS.includes(entity.email) ? ROLE_ADMIN : ROLE_CLIENT
 
-};
+		Role.find({ where: { name: roleName } }, (err, result) => {
+			if (err) {
+				return next(err)
+			}
+			const [clientRole] = result
+			if (!clientRole) {
+				return next(new Error('Could not find "client" role in DB.'))
+			}
+			clientRole.principals.create(
+				{
+					principalType: RoleMapping.USER,
+					principalId: entity.id,
+				},
+				(err) => {
+					if (err) {
+						return next(err)
+					}
+					console.log(`Role "${clientRole.name}" assigned to user "${entity.username}".`)
+					next()
+				}
+			)
+		})
+	})
+}
--- rest-api/server/boot/createRoles.js
+++ rest-api/server/boot/createRoles.js
@@ -1,63 +1,15 @@
-const ADMIN_EMAILS = ['admin@local.com']
+const ACCOUNT_ROLES = ['admin', 'client', 'editor']
 
-const assignRoleAdmin = (app, cb, role) => {
-	const { Account, RoleMapping, Role } = app.models
-
-	Account.find(
-		{
-			where: { email: { inq: ADMIN_EMAILS } },
-		},
-		(err, accounts) => {
-			if (err) {
-				console.error('searching for admins', err)
-				return cb(err)
-			}
-			accounts.forEach((account) => {
-				Role.isInRole(
-					role.name,
-					{ principalType: RoleMapping.USER, principalId: account.id },
-					(err, roleCheck) => {
-						if (err) {
-							return cb(err)
-						}
-						if (roleCheck) {
-							return
-						}
-						role.principals.create(
-							{
-								principalType: RoleMapping.USER,
-								principalId: account.id,
-							},
-							(err) => {
-								if (err) {
-									return cb(err)
-								}
-								console.log(`Role "${role.name}" assigned to user "${account.username}".`)
-							}
-						)
-					}
-				)
-			})
-			cb()
-		}
-	)
-}
-
-module.exports = (app, cb) => {
+module.exports = async (app, cb) => {
 	const { Role } = app.models
-	const adminRole = { name: 'admin' }
-	const clientRole = { name: 'client' }
 
-	Role.findOrCreate(clientRole, (err) => {
-		if (err) {
-			return cb(err)
+	for await (name of ACCOUNT_ROLES) {
+		try {
+			await Role.findOrCreate({ name })
+		} catch (err) {
+			cb(err)
 		}
-		Role.findOrCreate(adminRole, (err, role) => {
-			if (err) {
-				console.error('searching for admin role', err)
-				return cb(err)
-			}
-			assignRoleAdmin(app, cb, role)
-		})
-	})
+	}
+
+	cb()
 }
